9474909d83afc2b4bcd767ce9862cdd09719bb8e
kcmandable09 <kmandable@gmail.com>
2025-09-19 09:59:33 -0400

    update

--- src/run_tests.py
+++ src/run_tests.py
@@ -1,9 +1,9 @@
 from permutation1 import make_files1
 from permutation3 import make_files3
 
-make_files1(tot_n=9, max_decks=2, seed = 0)
+make_files1(tot_n=2000000, max_decks=10000, seed = 0)
 
-make_files3(tot_n=9, max_decks=2, seed = 0)
+make_files3(tot_n=2000000, max_decks=10000, seed = 0)
 
 
 
