b158c31e72032bcd5af47dd4450471ac9f534dff
kcmandable09 <kmandable@gmail.com>
2025-10-01 10:02:06 -0400

    Create #kiris scoring system.py

---
+++ src/#kiris scoring system.py
@@ -0,0 +1,25 @@
+#kiris scoring system
+
+
+#generate all the combinations of true and false when three are picked
+
+#compare two combinations to see which win
+#combinations cannot be the same for both players
+
+
+#count the number of tricks won
+
+#count the number of cards won
+
+
+
+#initial ideas:
+#- make a df with the combinations already stored then iterate by row to calculate the number of tricks and cards for each player
+#- use arrays instead to hold the info
+#- use a for loop to build the combinations instead of inserting myself
+#- use another package to make the table like tabulate
+#- make individual functions to calculate num of tricks or cards
+#- use previously made variable to calculate num of tricks or cards
+#- do i need to write the code to look into each file in the data folder then calculate?
+#- how can i best present to overall findings?
+#- need to make some kind of table, do i do this with df or tabulate or just normal npy file - this links back to the format of how we printed the info from all the different generate decks
